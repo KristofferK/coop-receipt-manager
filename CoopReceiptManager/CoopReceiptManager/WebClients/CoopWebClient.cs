4e30cee20c9c59557eff25e7755ddf7c394c6aae
KristofferK <kristoffer@minlejebolig.dk>
2018-03-14 00:45:41 +0100

    Fix CoopWebClient
    CoopWebClient had problems when methods were called multiple times, as they did not clean up after themselves.

--- CoopReceiptManager/CoopReceiptManager/WebClients/CoopWebClient.cs
+++ CoopReceiptManager/CoopReceiptManager/WebClients/CoopWebClient.cs
@@ -11,19 +11,26 @@ namespace CoopReceiptManager.WebClients
     {
         private CookieAwareWebClient webClient = new CookieAwareWebClient();
 
+        public CoopWebClient()
+        {
+            webClient.Headers.Add("User-Agent", "Coop Receipt Manager - https://github.com/KristofferK/coop-receipt-manager/");
+            webClient.Encoding = Encoding.UTF8;
+        }
+
         public void SignIn(CoopCredentials credentials)
         {
             var email = WebUtility.UrlEncode(credentials.Email);
             var password = WebUtility.UrlEncode(credentials.Password);
             var payload = $"EmailOrMemberNumber={email}&Password={password}&FacebookSignedRequest=&HashTag=&RedirectUrl=https%3A%2F%2Fcoop.dk%2F&OpenInNewWindow=False&ActiveType=EmailMemberNumber";
+            
+            webClient.Headers["Content-Type"] = "application/x-www-form-urlencoded";
+            webClient.Headers["Referer"] = "https://coop.dk/login";
+            webClient.Headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
 
-            webClient.Headers[HttpRequestHeader.UserAgent] = "Coop Receipt Manager - https://github.com/KristofferK/coop-receipt-manager/";
-            webClient.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
-            webClient.Headers[HttpRequestHeader.Referer] = "https://coop.dk/login";
-            webClient.Headers.Add("Origin", "https://coop.dk");
-            webClient.Headers.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
-            webClient.Encoding = Encoding.UTF8;
             var source = webClient.UploadString("https://coop.dk/login/", payload);
+            webClient.Headers.Remove("Referer");
+            webClient.Headers.Remove("Accept");
+            webClient.Headers.Remove("Content-Type");
 
             if (source.Contains("<h1>Log ind</h1>"))
             {
@@ -33,25 +40,31 @@ namespace CoopReceiptManager.WebClients
 
         public void GetReceipts()
         {
-            webClient.Headers[HttpRequestHeader.UserAgent] = "Coop Receipt Manager - https://github.com/KristofferK/coop-receipt-manager/";
-            webClient.Headers[HttpRequestHeader.Referer] = "https://medlem.coop.dk/medlemskonto+og+kvitteringer/Kvitteringer";
-            webClient.Headers.Add("Accept", "application/json, text/javascript, */*; q=0.01");
-            webClient.Headers.Add("X-Requested-With", "XMLHttpRequest");
+            webClient.Headers["Referer"] = "https://medlem.coop.dk/medlemskonto+og+kvitteringer/Kvitteringer";
+            webClient.Headers["Accept"] = "application/json, text/javascript, */*; q=0.01";
+            webClient.Headers["X-Requested-With"] = "XMLHttpRequest";
 
             var source = webClient.DownloadString("https://medlem.coop.dk/MemberAccount/GetReceipts?pageIndex=1&pageSize=25&fraDato=01-01-2010&tilDato=31-12-2100");
+            webClient.Headers.Remove("Referer");
+            webClient.Headers.Remove("Accept");
+            webClient.Headers.Remove("X-Requested-With");
+
             Console.WriteLine("Receipts: " + source);
         }
 
         public void GetReceipt(string receiptId)
         {
-            webClient.Headers[HttpRequestHeader.UserAgent] = "Coop Receipt Manager - https://github.com/KristofferK/coop-receipt-manager/";
-            webClient.Headers[HttpRequestHeader.Referer] = "https://medlem.coop.dk/medlemskonto+og+kvitteringer/Kvitteringer";
-            webClient.Headers.Add("X-fancyBox", "true");
-            webClient.Headers.Add("Accept", "text/html, */*; q=0.01");
-            webClient.Headers.Add("X-Requested-With", "XMLHttpRequest");
-            webClient.Headers.Remove(HttpRequestHeader.ContentType);
+            webClient.Headers["Referer"] = "https://medlem.coop.dk/medlemskonto+og+kvitteringer/Kvitteringer";
+            webClient.Headers["Accept"] = "text/html, */*; q=0.01";
+            webClient.Headers["X-Requested-With"] = "XMLHttpRequest";
+            webClient.Headers["X-fancyBox"] = "true";
 
             var source = webClient.DownloadString("https://medlem.coop.dk/MemberAccount/GetReceiptDetails?receiptId=" + receiptId);
+            webClient.Headers.Remove("Referer");
+            webClient.Headers.Remove("Accept");
+            webClient.Headers.Remove("X-Requested-With");
+            webClient.Headers.Remove("X-fancyBox");
+
             Console.WriteLine("Receipt: " + source);
         }
     }
